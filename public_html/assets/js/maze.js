f2a111cbdbffa432cecc1d5a276d1cdf27f709a4
Jordan Whistler <whistler.5@osu.edu>
2024-05-27 18:45:47 -0400

    try to fix css

--- public_html/assets/js/maze.js
+++ public_html/assets/js/maze.js
@@ -1,6 +1,5 @@
 const MAZE_ROWS = Math.floor(document.querySelector("#home").clientHeight);
 const MAZE_COLS = Math.floor(document.querySelector("#home").clientWidth) / 5;
-console.log(MAZE_ROWS, MAZE_COLS);
 const maze = document.querySelector(".maze");
 grid = new Array(MAZE_ROWS).fill(0).map(() => new Array(MAZE_COLS).fill(0));
 
@@ -51,84 +50,85 @@ function isValid(cell) {
     cell[1] < grid[0].length - 1
   );
 }
+if (maze) {
+  buildGrid();
 
-buildGrid();
+  let currentCell = [1, 1];
+  removeWall(currentCell[0], currentCell[1]);
+  getDocNode(currentCell[0], currentCell[1]).classList.add("visited");
+  grid[currentCell[0]][currentCell[1]] = 1;
+  let wall_list = [];
+  let list = neighbors(currentCell);
 
-let currentCell = [1, 1];
-removeWall(currentCell[0], currentCell[1]);
-getDocNode(currentCell[0], currentCell[1]).classList.add("visited");
-grid[currentCell[0]][currentCell[1]] = 1;
-let wall_list = [];
-let list = neighbors(currentCell);
-
-for (let i = 0; i < list.length; i++) {
-  if (
-    list[i][0] > 0 &&
-    list[i][0] < grid.length - 1 &&
-    list[i][1] > 0 &&
-    list[i][1] < grid[0].length - 1
-  ) {
-    wall_list.push(list[i]);
+  for (let i = 0; i < list.length; i++) {
+    if (
+      list[i][0] > 0 &&
+      list[i][0] < grid.length - 1 &&
+      list[i][1] > 0 &&
+      list[i][1] < grid[0].length - 1
+    ) {
+      wall_list.push(list[i]);
+    }
   }
-}
 
-const interval = window.setInterval(function () {
-  while (true) {
-    if (wall_list.length == 0) {
-      clearInterval(interval);
-      return;
-    }
-
-    let index = random_int(0, wall_list.length);
-    let wall = wall_list[index];
-    wall_list.splice(index, 1);
-    let cell_pair;
-
-    if (wall[0] % 2 == 0)
-      cell_pair = [
-        [wall[0] - 1, wall[1]],
-        [wall[0] + 1, wall[1]],
-      ];
-    else
-      cell_pair = [
-        [wall[0], wall[1] - 1],
-        [wall[0], wall[1] + 1],
-      ];
-
-    let new_cell;
-    let valid = false;
-
-    if (grid[cell_pair[0][0]][cell_pair[0][1]] < 1) {
-      new_cell = cell_pair[0];
-      valid = true;
-    } else if (grid[cell_pair[1][0]][cell_pair[1][1]] < 1) {
-      new_cell = cell_pair[1];
-
-      valid = true;
-    }
-
-    if (valid) {
-      if (isValid(wall)) {
-        getDocNode(wall[0], wall[1]).classList.add("visited");
-        removeWall(wall[0], wall[1]);
+  const interval = window.setInterval(function () {
+    while (true) {
+      if (wall_list.length == 0) {
+        clearInterval(interval);
+        return;
       }
-      if (isValid(new_cell)) {
-        getDocNode(new_cell[0], new_cell[1]).classList.add("visited");
-        removeWall(new_cell[0], new_cell[1]);
-      }
-      grid[new_cell[0]][new_cell[1]] = 1;
-      let list = neighbors(new_cell, 1);
 
-      for (let i = 0; i < list.length; i++)
-        if (
-          list[i][0] > 0 &&
-          list[i][0] < grid.length - 1 &&
-          list[i][1] > 0 &&
-          list[i][1] < grid[0].length - 1
-        ) {
-          wall_list.push(list[i]);
+      let index = random_int(0, wall_list.length);
+      let wall = wall_list[index];
+      wall_list.splice(index, 1);
+      let cell_pair;
+
+      if (wall[0] % 2 == 0)
+        cell_pair = [
+          [wall[0] - 1, wall[1]],
+          [wall[0] + 1, wall[1]],
+        ];
+      else
+        cell_pair = [
+          [wall[0], wall[1] - 1],
+          [wall[0], wall[1] + 1],
+        ];
+
+      let new_cell;
+      let valid = false;
+
+      if (grid[cell_pair[0][0]][cell_pair[0][1]] < 1) {
+        new_cell = cell_pair[0];
+        valid = true;
+      } else if (grid[cell_pair[1][0]][cell_pair[1][1]] < 1) {
+        new_cell = cell_pair[1];
+
+        valid = true;
+      }
+
+      if (valid) {
+        if (isValid(wall)) {
+          getDocNode(wall[0], wall[1]).classList.add("visited");
+          removeWall(wall[0], wall[1]);
         }
-      return;
+        if (isValid(new_cell)) {
+          getDocNode(new_cell[0], new_cell[1]).classList.add("visited");
+          removeWall(new_cell[0], new_cell[1]);
+        }
+        grid[new_cell[0]][new_cell[1]] = 1;
+        let list = neighbors(new_cell, 1);
+
+        for (let i = 0; i < list.length; i++)
+          if (
+            list[i][0] > 0 &&
+            list[i][0] < grid.length - 1 &&
+            list[i][1] > 0 &&
+            list[i][1] < grid[0].length - 1
+          ) {
+            wall_list.push(list[i]);
+          }
+        return;
+      }
     }
-  }
-}, 28);
+  }, 28);
+}
